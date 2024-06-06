47e1cc9756ba262dc5beaa77fa1ccf2adcfa8cc4
Najlae Abarghache <najlae.abarghache1@gmail.com>
2024-06-06 16:46:16 +0100

    Stats

--- Source/Aura/Public/UI/HUD/AuraHUD.h
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -6,6 +6,8 @@
 #include "GameFramework/HUD.h"
 #include "AuraHUD.generated.h"
 
+
+class UAttributeMenuWidgetController;
 class UAttributeSet;
 class UAbilitySystemComponent;
 class UOverlayWidgetController;
@@ -22,10 +24,10 @@ class AURA_API AAuraHUD : public AHUD
 
 public:
 
-	UPROPERTY()
-	TObjectPtr<UAuraUserWidget>  OverlayWidget;
-
 	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+
+	UFUNCTION(BlueprintCallable)
+	UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const FWidgetControllerParams& WCParams);
 
 	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
 
@@ -38,7 +40,18 @@ protected:
 	TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
 
 private:
+
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget>  OverlayWidget;
+
 	UPROPERTY()
 	TObjectPtr<UOverlayWidgetController> OverlayWidgetController;
+
+
+	UPROPERTY()
+	TObjectPtr<UAttributeMenuWidgetController> AttributeMenuWidgetController;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAttributeMenuWidgetController> AttributeMenuWidgetControllerClass;
 
 };
